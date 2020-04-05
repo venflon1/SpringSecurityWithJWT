0fa32204e825b695bbcd8b60f67cc240ca1acd49
Roberto <roberto.amato.11.93@gmail.com>
2020-04-05 12:59:01 +0200

    first-commit with pom.xml and class JwtProvide.java form managing create and decode JWT

---
+++ src/main/java/com/example/demo/jwt/DemoSpringSecurityJwtApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo.jwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSpringSecurityJwtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoSpringSecurityJwtApplication.class, args);
+	}
+
+}
